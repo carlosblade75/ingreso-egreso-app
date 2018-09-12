3612a7ba0c1bd7ed37dbc479f13c3f6a3feb873f
carlosblade75 <carlosblade75@hotmail.com>
2018-09-12 14:08:25 +0200

    Primer commit ready

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,16 @@
+
+import { Routes } from '@angular/router';
+
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+import { EstadisticaComponent } from '../ingreso-egreso/estadistica/estadistica.component';
+
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+import { DashboardComponent } from './dashboard.component';
+
+
+export const dashboardRoutes: Routes = [
+    { path: 'detalle', component: DetalleComponent},
+    { path: 'ingesoegreso', component: IngresoEgresoComponent},
+    { path: '', component: EstadisticaComponent},
+
+];
